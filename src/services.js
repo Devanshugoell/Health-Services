d550cb71f40240c03c64f26474fe5ff3cbaf5ec8
Devanshu <devgoyal952@gmail.com>
2024-09-27 17:42:00 +0530

    Added Health Services

---
+++ src/services.js
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    id: 1,
+    name: "General Consultation",
+    Description: "To discuss health concerns.",
+    Price: 500,
+  },
+  {
+    id: 2,
+    name: "Blood Test",
+    Description: "To analysis of a blood sample ",
+    Price: 200,
+  },
+  {
+    id: 3,
+    name: "X-Ray",
+    Description: "To view the inside of the body",
+    Price: 500,
+  },
+  {
+    id: 4,
+    name: "MRI Scan",
+    Description: "To create  images of organ",
+    Price: 5000,
+  },
+  {
+    id: 5,
+    name: "Ultrasound",
+    Description: "To create images of the inside of the body",
+    Price: 1000,
+  },
+  {
+    id: 6,
+    name: "Vaccination",
+    Description: "Administration of a vaccine",
+    Price: 500,
+  },
+  {
+    id: 7,
+    name: "Dental Cleaning",
+    Description: "To clean of teeth",
+    Price: 800,
+  },
+  {
+    id: 8,
+    name: "Eye Examination",
+    Description: "To check-up eyes",
+    Price: 500,
+  },
+  {
+    id: 9,
+    name: "Surgery",
+    Description: "Surgical removal of the appendix",
+    Price: 30000,
+  },
+];
